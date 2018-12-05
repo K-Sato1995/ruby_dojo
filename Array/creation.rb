e71ef12be7d777d8bfa59891b627cf1e326cc350
Katsuki <Katsuki_Sato0130@icloud.com>
2018-12-06 07:48:08 +0900

    Added Array

---
+++ Array/creation.rb
@@ -0,0 +1,21 @@
+# Creating arrays
+
+## (1) []
+arr = [1, 2, 3]
+p arr #=> [1, 2, 3]
+
+## (2) Array
+arr2 = Array[1, 2, 3]
+p arr2 #=> [1, 2, 3]
+
+## (3) Array.new(arg1, arg2)
+arr3 = Array.new(3, 'string')
+p arr3 #=> ["string", "string", "string"]
+
+## (4) Array.new([])
+arr4 = Array.new([1, 2, 3])
+p arr4 #=> [1, 2, 3]
+
+## (5)Array.new(num) {}
+arr5 = Array.new(3) { |a| a * 3 }
+p arr5 #=> [0, 3, 6]
